b5e4bd9fff898e16489c947df4315c61c1de6bfa
Tomasz Sterna <tomek@xiaoka.com>
2017-02-23 16:58:15 +0100

    jabber:iq:roster remove and error handling

--- modules/roster.js
+++ modules/roster.js
@@ -29,6 +29,14 @@ module.exports = function (router) {
   return function roster (req, res, next) {
     var query, items, item
     if (req.is('iq') && (query = req.getChild('query', 'jabber:iq:roster'))) {
+      // https://xmpp.org/rfcs/rfc6121.html#roster-add-errors
+      if (req.to !== xmpp.JID(req.attrs.from).bare().toString()) {
+        return next(new StanzaError(
+          'not allowed to access roster',
+          'auth',
+          'forbidden'
+        ))
+      }
       items = query.getChildren('item')
       if (req.attrs.type === 'get') {
         if (items.length) {
@@ -44,13 +52,19 @@ module.exports = function (router) {
             else {
               query = res.c('query', {
                 xmlns: 'jabber:iq:roster',
-                ver: new Date().toISOString() // FIXME!
+                // FIXME!
+                ver: new Date().toISOString()
               })
 
               for (item of items) {
-                query.c('item', { jid: item.jid, name: item.name })
+                let el = query.c('item', { jid: item.jid })
+                if (item.name) el.attrs.name = item.name
               }
-
+              // TODO
+              // 2.1.2.1.  Approved Attribute
+              // 2.1.2.2.  Ask Attribute
+              // 2.1.2.5.  Subscription Attribute
+              // 2.1.2.6.  Group Element
               res.send()
             }
           })
@@ -59,21 +73,36 @@ module.exports = function (router) {
         }
       }
       if (req.attrs.type === 'set') {
+        // https://xmpp.org/rfcs/rfc6121.html#roster-add-errors
+        if (items.length !== 1) {
+          return next(new StanzaError(
+            'roster-set can contain one item only',
+            'modify',
+            'bad-request'
+          ))
+        }
+        if (!(item = items[0]).attrs.jid) {
+          return next(new StanzaError(
+            'roster-set requires jid for item`',
+            'modify',
+            'bad-request'
+          ))
+        }
         try {
-          let updates = items.map(item => {
-            return new Promise((resolve, reject) => {
-              Roster.update(
-                { User: req.to, jid: item.attrs.jid },
-                { name: item.attrs.name },
-                err => {
-                  if (err) reject(err)
-                  else resolve()
-                }
-              )
-            })
-          })
-          Promise.all(updates).catch(next).then(() => res.send())
-          // FIXME implement push
+          let cb = err => {
+            if (err) next(err)
+            else res.send()
+          }
+          let key = { User: req.to, jid: item.attrs.jid }
+          if (item.attrs.subscription === 'remove') {
+            Roster.delete(key, cb)
+          } else {
+            Roster.update(key, { name: item.attrs.name }, cb)
+          }
+          // TODO
+          // 2.1.2.6.  Group Element
+          // FIXME 2.1.6.  Roster Push
+          // If a connected resource or available resource requests the roster, it is referred to as an "interested resource". The server MUST send roster pushes to all interested resources.
         } catch (e) {
           next(e)
         }
--- test/roster.js
+++ test/roster.js
@@ -61,8 +61,51 @@ test('roster get empty', async t => {
   t.truthy(err2.getChild('bad-request', 'urn:ietf:params:xml:ns:xmpp-stanzas'))
 })
 
-test('roster set/get', async t => {
+test('roster set invalid', async t => {
   t.plan(15)
+
+  const get1 = iq('get')
+  get1.root().attrs.from = 'some@otherhost/hacker'
+  const res1 = await router.iq(get1)
+  t.is(res1.attrs.type, 'error')
+  t.is(res1.attrs.id, 'get_1')
+  const err1 = res1.getChild('error')
+  t.is(err1.attrs.type, 'auth')
+  const set2 = iq('set', 2).c('item', { jid: 'one@example.com' })
+  set2.root().attrs.from = 'some@otherhost'
+  const res2 = await router.iq(set2)
+  t.is(res2.attrs.type, 'error')
+  t.is(res2.attrs.id, 'set_2')
+  const err2 = res2.getChild('error')
+  t.is(err2.attrs.type, 'auth')
+
+  const set3 = iq('set', 3)
+  const res3 = await router.iq(set3)
+  t.is(res3.attrs.type, 'error', 'no item')
+  t.is(res3.attrs.id, 'set_3')
+  const err3 = res3.getChild('error')
+  t.is(err3.attrs.type, 'modify')
+
+  const set4 = iq('set', 4)
+  set4.c('item', { jid: 'one@example.com' })
+  set4.c('item', { jid: 'two@example.com' })
+  const res4 = await router.iq(set4)
+  t.is(res4.attrs.type, 'error', 'too many items')
+  t.is(res4.attrs.id, 'set_4')
+  const err4 = res4.getChild('error')
+  t.is(err4.attrs.type, 'modify')
+
+  const set5 = iq('set', 5).c('item', {})
+  const res5 = await router.iq(set5)
+  t.is(res5.attrs.type, 'error', 'no jid')
+  t.is(res5.attrs.id, 'set_5')
+  const err5 = res5.getChild('error')
+  t.is(err5.attrs.type, 'modify')
+  // TODO repeating group, empty group
+})
+
+test('roster set/get/update/delete', async t => {
+  t.plan(41)
 
   const set1 = iq('set').c('item', { jid: 'one@example.com', name: 'One' })
   const res1 = await router.iq(set1)
@@ -77,6 +120,7 @@ test('roster set/get', async t => {
   t.truthy(query2.attrs.ver)
   const items2 = query2.getChildren('item')
   t.is(items2.length, 1)
+
   const set3 = iq('set', 3).c('item', { jid: 'two@example.com' })
   const res3 = await router.iq(set3)
   t.is(res3.attrs.type, 'result')
@@ -90,5 +134,65 @@ test('roster set/get', async t => {
   t.truthy(query4.attrs.ver)
   const items4 = query4.getChildren('item')
   t.is(items4.length, 2)
-  t.deepEqual(new Set(items4.map(item => item.attrs.name)), new Set([ 'One', undefined ]))
+  t.deepEqual(new Set(items4.map(item => item.attrs.name)), new Set([
+    'One',
+    undefined
+  ]))
+
+  const set5 = iq('set', 5).c('item', { jid: 'two@example.com', name: 'Two' })
+  const res5 = await router.iq(set5)
+  t.is(res5.attrs.type, 'result')
+  t.is(res5.attrs.id, 'set_5')
+  const set6 = iq('set', 6).c('item', { jid: 'one@example.com' })
+  const res6 = await router.iq(set6)
+  t.is(res6.attrs.type, 'result')
+  t.is(res6.attrs.id, 'set_6')
+  const get7 = iq('get', 7)
+  const res7 = await router.iq(get7)
+  t.is(res7.attrs.type, 'result')
+  t.is(res7.attrs.id, 'get_7')
+  const query7 = res7.getChild('query', 'jabber:iq:roster')
+  t.truthy(query7)
+  t.truthy(query7.attrs.ver)
+  const items7 = query7.getChildren('item')
+  t.is(items7.length, 2)
+  t.deepEqual(new Set(items7.map(item => item.attrs.name)), new Set([
+    'Two',
+    undefined
+  ]))
+  const set8 = iq('set', 8).c('item', { jid: 'two@example.com', name: '' })
+  const res8 = await router.iq(set8)
+  t.is(res8.attrs.type, 'result')
+  t.is(res8.attrs.id, 'set_8')
+  const get9 = iq('get', 9)
+  const res9 = await router.iq(get9)
+  t.is(res9.attrs.type, 'result')
+  t.is(res9.attrs.id, 'get_9')
+  const query9 = res9.getChild('query', 'jabber:iq:roster')
+  t.truthy(query9)
+  t.truthy(query9.attrs.ver)
+  const items9 = query9.getChildren('item')
+  t.is(items9.length, 2)
+  t.deepEqual(new Set(items9.map(item => item.attrs.name)), new Set([
+    undefined,
+    undefined
+  ]))
+
+  const setA = iq('set', 'A').c('item', {
+    jid: 'one@example.com',
+    subscription: 'remove'
+  })
+  const resA = await router.iq(setA)
+  t.is(resA.attrs.type, 'result')
+  t.is(resA.attrs.id, 'set_A')
+  const getB = iq('get', 'B')
+  const resB = await router.iq(getB)
+  t.is(resB.attrs.type, 'result')
+  t.is(resB.attrs.id, 'get_B')
+  const queryB = resB.getChild('query', 'jabber:iq:roster')
+  t.truthy(queryB)
+  t.truthy(queryB.attrs.ver)
+  const itemsB = queryB.getChildren('item')
+  t.is(itemsB.length, 1)
+  t.is(itemsB[0].attrs.jid, 'two@example.com')
 })
